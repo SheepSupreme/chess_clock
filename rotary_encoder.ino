f821d65b70819bda2f03fc6d805b185463d2e868
lukas <61695507+SheepSupreme@users.noreply.github.com>
2023-10-11 16:24:38 +0200

    button press snippet

--- rotary_encoder.ino
+++ rotary_encoder.ino
@@ -19,21 +19,27 @@ static uint16_t store=0;
 
 void loop() {
 static int8_t c,val;
-
-   if( val=read_rotary() ) {
-      c +=val;
-      Serial.println(val);Serial.print(" ");
+  if(digitalRead(SW)==0){
+    delay(10);
+    Serial.println("pressed");
+    while(digitalRead(SW==0)){
+      if(digitalRead(SW)==1){
+        delay(10);
+        return 0;
+      }
+      }
+  }
+  if( val=read_rotary() ) {
+     c +=val;
+     Serial.println(val);Serial.print(" ");
    }
 }
 
 // A vald CW or  CCW move returns 1, invalid returns 0.
 int8_t read_rotary() {
   static int8_t rot_enc_table[] = {0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0};
-  
+  ß
   prevNextCode <<= 2;
-  if (digitalRead(SW)){
-    Serial.println(0);
-  }
   if (digitalRead(DATA)) prevNextCode |= 0x02;
   if (digitalRead(CLK)) prevNextCode |= 0x01;
   prevNextCode &= 0x0f;
