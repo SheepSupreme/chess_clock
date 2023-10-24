c24fb06576201dbf95d207f8c593fa3565f01828
David <61849332+DeeQay@users.noreply.github.com>
2023-10-24 10:01:21 +0200

    Add files via upload

--- 7-segment.ino
+++ 7-segment.ino
@@ -12,10 +12,10 @@
 #define BUTTON1 10
 
 //------------------ Variablen ---------------------------------------
-int time0 = 300;
-int time1 = 600;
+int playerTime[2];
 
 bool currentPlayer = 0;
+bool timerActive = true;
 
 struct Segment {
   uint8_t numbers[10] = {
@@ -41,97 +41,103 @@ Segment segment;
 // Interrupt Service Routine
 ISR(TIMER1_COMPA_vect)
 {
-  if (currentPlayer == 0) {
-    time0--;
+  if (!timerActive) {
+    return;
   }
-  if (currentPlayer == 1) {
-    time1--;
+
+  playerTime[currentPlayer]--;
+
+  displaySegmentInt(currentPlayer, playerTime[currentPlayer]);
+
+  // check ob jemand verloren hat
+  if (playerTime[currentPlayer] <= 0) {
+    gameOver(currentPlayer);
   }
-  
-  clearDisplay();
-  displaySegmentInt(0, time0);
-  displaySegmentInt(1, time1);
 }
 
 void timerSetup() {
   // Timer1 im CTC-Modus auf 1 Sekunde einstellen
   noInterrupts(); // alle Interrupts abschalten cli()
-  TCCR1A=0; // Registereintrag löschen
-  TCCR1B=0; // Registereintrag löschen
-  TCCR1B|=(1<<CS12); // 256 als Prescale-Wert spezifizieren
-  TCCR1B|=(1<<WGM12); // Setzt das Bit für den CTC-Mode
-  OCR1A=62499; // Auf Endwert setzen 62500 Impulse zählen = 1 Sekunde
-  TIMSK1|=(1<<OCIE1A); // Schaltet den Compare-Interrupt ein
+  TCCR1A = 0; // Registereintrag löschen
+  TCCR1B = 0; // Registereintrag löschen
+  TCCR1B |= (1 << CS12); // 256 als Prescale-Wert spezifizieren
+  TCCR1B |= (1 << WGM12); // Setzt das Bit für den CTC-Mode
+  OCR1A = 62499; // Auf Endwert setzen 62500 Impulse zählen = 1 Sekunde
+  TIMSK1 |= (1 << OCIE1A); // Schaltet den Compare-Interrupt ein
   interrupts(); // Alle Interrupts scharf schalten sei()
 }
 
 void displaySetup() {
-  senden(0x0B07); // Scan Limit Register
+  sendMux(0x0B07); // Scan Limit Register
   delay(10);
-  senden(0x0A0F); // Intensity (Helligkeit)
+  sendMux(0x0A0F); // Intensity (Helligkeit)
   delay(10);
-  senden(0x0C01); // Shutdown Register
+  sendMux(0x0C01); // Shutdown Register
   delay(10);
-  senden(0x0900); // Decode Mode
+  sendMux(0x0900); // Decode Mode
   delay(10);
 
   clearDisplay();
 }
 
 /*
-Anzeige von `data` auf der 7Seg-Anz an der Position `pos`.
+  Anzeige von `data` auf der 7Seg-Anz an der Position `pos`.
 
-pos: 1 steht für die erste 7Seg-Anz, 8 für die letzte.
+  pos: 1 steht für die erste 7Seg-Anz, 8 für die letzte.
 */
-void displaySegment(int pos, uint8_t data) {
+void displaySegment(int pos, int data) {
   data += (9 - pos) * 0x0100;
-  senden(data);
+  sendMux(data);
 }
 
 /*
-Anzeige einer Zahl auf einer Seite der 7Seg-Anz.
+  Anzeige einer Zahl auf einer Seite der 7Seg-Anz.
 
-leftPos: true, um die Zahl auf den ersten vier 7Seg-Anz darzustellen.
-leftPos: false, um die Zahl auf den 5. bis 8. 7Seg-Anz darzustellen.
+  leftPos: true, um die Zahl auf den ersten vier 7Seg-Anz darzustellen.
+  leftPos: false, um die Zahl auf den 5. bis 8. 7Seg-Anz darzustellen.
 
-num: Die Zahl die angezeigt werden soll (maximal 4 Ziffern).
+  num: Die Zahl die angezeigt werden soll (maximal 4 Ziffern).
 */
 void displaySegmentInt(bool leftPos, int num) {
-    const int maxDigits = 4; // 4 7Seg-Anz pro Seite
-    int digits[maxDigits];
+  const int maxDigits = 4; // 4 7Seg-Anz pro Seite
+  int digits[maxDigits];
 
-    int count = 0;
+  int count = 0;
 
-    // Jede Ziffer einzeln speichern
-    while (num > 0 && count < maxDigits) {
-        int digit = num % 10; // Letzte Ziffer der Zahl
-        digits[count] = digit;
-        count++;
-        num /= 10; // Letzte Ziffer entfernen
+  // Jede Ziffer einzeln speichern
+  while (num > 0 && count < maxDigits) {
+    int digit = num % 10; // Letzte Ziffer der Zahl
+    digits[count] = digit;
+    count++;
+    num /= 10; // Letzte Ziffer entfernen
+  }
+
+  // Reverse array
+  for (int i = 0; i < count / 2; i++) {
+    int temp = digits[i];
+    digits[i] = digits[count - 1 - i];
+    digits[count - 1 - i] = temp;
+  }
+
+  // Anzeige
+  for (int i = 0; i < 4; i++) {
+    int displayPos = i + 1;
+    if (!leftPos) {
+      displayPos += maxDigits; // Für Rechte Seite
     }
-
-    // Reverse array
-    for (int i = 0; i < count / 2; i++) {
-        int temp = digits[i];
-        digits[i] = digits[count - 1 - i];
-        digits[count - 1 - i] = temp;
+    if (i < count) {
+      displaySegment(displayPos, segment.numbers[digits[i]]); // Ziffer anzeigen
+    } else {
+      displaySegment(displayPos, 0); // clear
     }
-
-    for (int i = 0; i < count; i++) {
-        int displayPos = i + 1;
-        if (!leftPos) {
-          displayPos += maxDigits; // Für Rechte Seite
-        }
-        displaySegment(displayPos, segment.numbers[digits[i]]);
-    }
+  }
 }
 
 // Kommunikation mit MUX7219
-void senden(unsigned int data) {
+void sendMux(unsigned int data) {
   digitalWrite(CS, LOW);
   bool zw;
   unsigned int maske = 0x8000;
-
 
   maske = 0x8000;
   for (int i = 0; i < 16; i++) {
@@ -147,7 +153,14 @@ void senden(unsigned int data) {
 // Anzeige löschen
 void clearDisplay() {
   for (int i = 1; i < 9; i++) {
-    displaySegment(i, 0x0000);
+    displaySegment(i, 0);
+  }
+}
+
+void gameOver(int lostPlayer) {
+  timerActive = false;
+  for (int i = 1; i < 9; i++) {
+    displaySegment(i, segment.numbers[lostPlayer]);
   }
 }
 
@@ -160,6 +173,9 @@ void setup() {
   Serial.begin(9600);
   Serial.println("start");
 
+  playerTime[0] = 10;
+  playerTime[1] = 10;
+  
   timerSetup();
   displaySetup();
 }
